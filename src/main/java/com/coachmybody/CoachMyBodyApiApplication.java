46b92e61151b960858d8b8dd9d7e991b8a439a7d
hongbeom <wowow1859@naver.com>
2021-03-11 00:42:14 +0900

    Initial Project

---
+++ src/main/java/com/coachmybody/CoachMyBodyApiApplication.java
@@ -0,0 +1,13 @@
+package com.coachmybody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachMyBodyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachMyBodyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coachmybody/CoachMyBodyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coachmybody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachMyBodyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
