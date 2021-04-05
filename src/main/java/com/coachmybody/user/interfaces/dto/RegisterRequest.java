1c48d0cb34177cb59ea20bcc17c0ddf769c90e3a
hongbeom <wowow1859@naver.com>
2021-04-06 01:35:01 +0900

    fix: [#1] 회원가입 request null check 수정

--- src/main/java/com/coachmybody/user/interfaces/dto/RegisterRequest.java
+++ src/main/java/com/coachmybody/user/interfaces/dto/RegisterRequest.java
@@ -1,13 +1,13 @@
 package com.coachmybody.user.interfaces.dto;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 import com.coachmybody.user.type.LoginType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.NonNull;
 import lombok.Value;
 
 @ApiModel(description = "회원가입 요청")
@@ -15,14 +15,16 @@ import lombok.Value;
 public class RegisterRequest {
 
 	@ApiModelProperty(value = "아이디", required = true)
+	@NotNull(message = "The socialId must not be Null")
 	String socialId;
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	@ApiModelProperty(value = "로그인 타입", example = "KAKAO", required = true)
+	@NotNull(message = "The loginType must not be Null")
 	LoginType loginType;
 
 	@ApiModelProperty(value = "닉네임", required = true)
-	@NonNull
+	@NotNull(message = "The nickname must not be Null")
 	String nickname;
 
 	@ApiModelProperty(value = "이메일", required = true)
